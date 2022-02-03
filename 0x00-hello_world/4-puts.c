a92a15d245530c8c3d820cef09949a02b7b86dc4
jgoyret <j.goyret1475@gmail.com>
2022-02-03 19:20:34 +0000

    4_puts_01

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - description of main function 4-puts exercise
+ *
+ * Return: always return number 0
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	/*
+	 * using puts
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
