7ca311bc5c8301679035f5057896347e5c2333a5
jgoyret <j.goyret1475@gmail.com>
2022-02-04 12:39:32 +0000

    5_printf_02

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+
+/**
+ * main - using printf with line break
+ *
+ * Return: Always return 0
+ *
+ */
 int main(void)
 {
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
