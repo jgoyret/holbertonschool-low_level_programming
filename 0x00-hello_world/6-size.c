b93cc38a1aadc029c0c609715f96465209b279db
jgoyret <j.goyret1475@gmail.com>
2022-02-04 14:03:33 +0000

    6_size_betty_correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,6 @@
  */
 int main(void)
 {
-	/*
-	 * using sizeof
-	 *
-	 */
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
