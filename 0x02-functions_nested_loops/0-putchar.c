786725eadde967705177f9e18f7da405620449de
jgoyret <j.goyret1475@gmail.com>
2022-02-07 15:54:23 +0000

    0_putchar_funciones_betty1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,17 @@
  * main - Print _putchar
  *
  * Return: Always 0
- **/ 
+ **/
+
 int main(void)
 {
 	int i;
 	char palabra[] = "_putchar";
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(palabra[i]);
 	}
 	_putchar('\n');
-	return 0;
+	return (0);
 }
