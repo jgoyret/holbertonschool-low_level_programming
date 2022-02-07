ab0ef0552647e23fbd9b795c03874b5d5f459758
jgoyret <j.goyret1475@gmail.com>
2022-02-07 18:06:41 +0000

    3_islower_01_betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- *  _islower - check if input of function is lower case 
+ * _islower - check if input c of function is lower case
  *
- *  Return: variable estado that is 1 if lowercase true and 0 if not
+ * c - a character that the user provides
+ *
+ * Return: variable estado that is 1 if lowercase true and 0 if not
  **/
 
 int _islower(int c)
 {
 
-	if ( c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
-		return(1);
-	} else 
+		return (1);
+	}
+	else
 	{
 		return (0);
 	}
